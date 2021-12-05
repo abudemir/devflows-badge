9e221e70f04cd5b4ae49ab4043ca80c2b5e30755
mskabapersonal <marcelo.skaba@gmail.com>
2021-12-05 10:39:48 +0000

    boto3

--- src/echo/index.py
+++ src/echo/index.py
@@ -11,13 +11,11 @@ logger.setLevel(logging.INFO)
 Write your code here.
 """
 def handler(event, context):
+
     logger.info("Context enabled Echo invocable")
     try:
         body = json.loads(event)['body']
-        context = json.loads(event).get('context', {})
-        flow_execution_id = context.get('flow_execution_id', None)
-        if not flow_execution_id:
-            return get_error_output(400, "Flow Execution ID (flow_execution_id) not specified in context")
+        result = list()
         return get_success_output(200, body)
     except:
         return get_error_output(500, "Dummy error message", "Internal Server Error")
