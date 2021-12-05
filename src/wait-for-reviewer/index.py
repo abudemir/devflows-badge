302b02c0def6eb27961e664a8ee6d1512b93500b
mskabapersonal <marcelo.skaba@gmail.com>
2021-12-05 12:49:13 +0000

    wait

---
+++ src/wait-for-reviewer/index.py
@@ -0,0 +1,24 @@
+import json
+import logging
+
+from src.output_formatter import get_error_output
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+"""
+Write your code here.
+"""
+def handler(event, context):
+    try:
+        body = json.loads(event)['body']
+        response = {
+            'statusCode': 200,
+            'request_id': body['request_id'],
+            'body': body
+        }
+
+        return response
+    except:
+        return get_error_output(500, "Dummy error message", "Internal Server Error")
