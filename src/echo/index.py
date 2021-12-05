bc889be6eddac9cdce59b22fd6eb2a019d0144ee
mskabapersonal <marcelo.skaba@gmail.com>
2021-12-05 10:52:27 +0000

    2nd version

--- src/echo/index.py
+++ src/echo/index.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import boto3
 
 from src.output_formatter import get_success_output, get_error_output
 
@@ -16,6 +17,21 @@ def handler(event, context):
     try:
         body = json.loads(event)['body']
         result = list()
-        return get_success_output(200, body)
+
+        client = boto3.client('translate', region_name="us-east-1")
+
+        for lang in body["translationLanguages"]:
+            print(lang)
+            resp = client.translate_text(
+                Text=body["text"], 
+                SourceLanguageCode=body["originalLanguageCode"], 
+                TargetLanguageCode=lang
+            )
+            result.insert({
+            "translatedText" : resp["TranslatedText"],
+            "language": lang
+            })
+
+        return get_success_output(200, {"result": result})
     except:
         return get_error_output(500, "Dummy error message", "Internal Server Error")
